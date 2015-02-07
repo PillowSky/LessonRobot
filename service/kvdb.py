d4daf8bc11405aa2f2c0b61c8128f635ccd46e8e
Yuzo <hyzgog@gmail.com>
2015-02-07 21:38:10 +0800

    implement a dummy kvdb with dict to re-support run stand alone

--- service/kvdb.py
+++ service/kvdb.py
@@ -1,3 +1,11 @@
-from sae.kvdb import KVClient
-
-kvdb = KVClient()
+try:
+	from sae.kvdb import KVClient
+except ImportError as e:
+	class KVClient(object):
+		cache = {}
+		def get(self, key):
+			return self.cache.get(key)
+		def set(self, key, value):
+			self.cache[key] = value
+finally:
+	kvdb = KVClient()
