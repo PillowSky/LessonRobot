b669ac820fe67569c7726eedafc8d268978acc84
PillowSky <pillowsky@qq.com>
2016-02-21 21:54:29 +0800

    improve selector

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from index import application
 
 port = 8000
 application.listen(port)
-print("Tornado Server Listen on port {}".format(port))
+print("Tornado server listen on port {}".format(port))
 IOLoop.instance().start()
