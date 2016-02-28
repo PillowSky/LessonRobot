e68809c4176e772a4aac3c15706d414ff532469b
PillowSky <pillowsky@qq.com>
2016-02-29 03:11:41 +0800

    add conf

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tornado.ioloop import IOLoop
 from index import application
 
-port = 8000
+port = 8888
 application.listen(port)
 print("Tornado server listen on port {}".format(port))
 IOLoop.instance().start()
