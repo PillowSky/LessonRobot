79307bb07c43d5b4808b04e842963a70243a54f9
Yuzo <hyzgog@gmail.com>
2016-03-03 00:28:58 +0800

    deploy to sae docker

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import os
 from tornado.ioloop import IOLoop
 from index import application
 
-port = 8888
+port = int(os.getenv('PORT', 8000))
 application.listen(port)
 print("Tornado server listen on port {}".format(port))
 IOLoop.instance().start()
