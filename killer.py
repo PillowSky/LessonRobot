ed64a3634307737f18ad7c768e095a3b948ad633
Yuzo <hyzgog@gmail.com>
2016-03-20 10:35:24 +0800

    tweak params

--- killer.py
+++ killer.py
@@ -1,59 +1,56 @@
-from tornado.gen import coroutine
+from tornado.gen import coroutine, sleep
 from tornado.ioloop import IOLoop
 from tornado.queues import Queue
-from tornado.httpclient import HTTPError
 from lessonrobot import LessonRobot
 
-concurrency = 10
-q = Queue(maxsize=concurrency)
-
+concurrency = 250
+q = Queue(maxsize=10)
 
 @coroutine
 def consumer():
-    while True:
-        username = yield q.get()
-        robot = LessonRobot()
-        result = yield robot.login(username, '123456')
+	while True:
+		username = yield q.get()
 
-        if result:
-            print('[Login] %s' % username)
-            try:
-                count = yield robot.page_count()
-                for i in xrange(count + 1):
-                    course_list = yield robot.page(i)
-                    print('[Page] %s: %d/%d => %d' % (username, i, count, len(course_list)))
-                    for course in course_list:
-                        print('[Learn] %s: %s' % (username, course))
-                        result = yield robot.learn(course)
-                        if result:
-                            print('[Ok] %s: %s' % (username, course))
-                        else:
-                            print('[More] %s: %s' % (username, course))
-                print('[Done] %s' % username)
-            except HTTPError as e:
-                print('[HTTPError] %s' % username)
-                yield q.put(username)
-        else:
-            print('[Invalid] %s' % username)
+		try:
+			robot = LessonRobot()
+			result = yield robot.login(username, '123456')
+			if result:
+				print('[Login] %s' % username)
+				count = yield robot.page_count()
+				for i in xrange(count + 1):
+					course_list = yield robot.page(i)
+					print('[Page] %s: %d/%d => %d' % (username, i, count, len(course_list)))
+					for course in course_list:
+						print('[Learn] %s: %s' % (username, course))
+						result = yield robot.learn(course)
+						if result:
+							print('[Ok] %s: %s' % (username, course))
+						else:
+							print('[More] %s: %s' % (username, course))
+				print('[Done] %s' % username)
+			else:
+				print('[Failed] %s' % username)
+		except Exception as e:
+			print('[Exception] %s:%s' % (username, e))
+			yield q.put(username)
 
-        q.task_done()
-
+		q.task_done()
 
 @coroutine
 def producer():
-    for i in xrange(200000, 200004):
-        username = 'sxce%06d' % i
-        yield q.put(username)
-        print('[Start] %s' % username)
-
+	for i in xrange(200000, 201000):
+		username = 'sxce%06d' % i
+		yield q.put(username)
+		print('[Start] %s' % username)
+		yield sleep(1)
 
 @coroutine
 def main():
-    for i in xrange(concurrency):
-        IOLoop.current().spawn_callback(consumer)
+	for i in xrange(concurrency):
+		IOLoop.current().spawn_callback(consumer)
 
-    yield producer()
-    yield q.join()
-    print('All Done')
+	yield producer()
+	yield q.join()
+	print('All Done')
 
 IOLoop.current().run_sync(main)
