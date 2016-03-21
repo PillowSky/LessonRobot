e7f10b153b67b4df7b98167a55d8bd8277009bed
Yuzo <hyzgog@gmail.com>
2016-03-21 22:37:23 +0800

    use logger

--- killer.py
+++ killer.py
@@ -1,38 +1,43 @@
 import signal
+import logging
 from tornado.gen import coroutine, sleep
 from tornado.ioloop import IOLoop
 from tornado.queues import Queue
+from tornado.httpclient import AsyncHTTPClient
 from lessonrobot import LessonRobot
 
 concurrency = 250
 q = Queue(maxsize=10)
 
+logging.basicConfig(format="%(asctime)s: %(message)s", level=logging.NOTSET)
+AsyncHTTPClient.configure(None, max_clients=1000)
+
 @coroutine
 def consumer():
 	while True:
 		username = yield q.get()
-		print('[Get] %s' % username)
+		logging.info('[Get] %s' % username)
 
 		try:
 			robot = LessonRobot()
 			result = yield robot.login(username, '123456')
 			if result:
-				print('[Login] %s' % username)
+				logging.info('[Login] %s' % username)
 				count = yield robot.page_count()
 				for i in xrange(count + 1):
 					course_list = yield robot.page(i)
 					course_len = len(course_list)
-					print('[Page] %s: %d/%d => %d' % (username, i, count, course_len))
+					logging.info('[Page] %s: %d/%d => %d' % (username, i, count, course_len))
 					if course_len == 0 and i != 0:
 						raise Exception('Session Expired')
 					for course in course_list:
-						print('[Learn] %s: %s' % (username, course))
+						logging.info('[Learn] %s: %s' % (username, course))
 						yield robot.learn(course)
-				print('[Done] %s' % username)
+				logging.info('[Done] %s' % username)
 			else:
-				print('[Failed] %s' % username)
+				logging.info('[Failed] %s' % username)
 		except Exception as e:
-			print('[Exception] %s:%s' % (username, e))
+			logging.info('[Exception] %s:%s' % (username, e))
 			yield q.put(username)
 
 		q.task_done()
@@ -42,12 +47,12 @@ def producer():
 	for i in xrange(200000, 201000):
 		username = 'sxce%06d' % i
 		yield q.put(username)
-		print('[Put] %s' % username)
+		logging.info('[Put] %s' % username)
 		yield sleep(1)
 
 def handler(signum, frame):
 	step = 10
-	print('[Signal] %d: concurrency += %d' % (signum, step))
+	logging.info('[Signal] %d: concurrency += %d' % (signum, step))
 	for i in xrange(step):
 		IOLoop.current().spawn_callback(consumer)
 
@@ -60,6 +65,6 @@ def main():
 
 	yield producer()
 	yield q.join()
-	print('All Done')
+	logging.info('All Done')
 
 IOLoop.current().run_sync(main)
