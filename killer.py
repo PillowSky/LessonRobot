754e1552e65b0a2380a4a9c19e8090c8f00eeb20
Yuzo <hyzgog@gmail.com>
2016-03-26 11:21:00 +0800

    submit region

--- killer.py
+++ killer.py
@@ -8,77 +8,31 @@ from tornado.httpclient import AsyncHTTPClient
 from lessonrobot import LessonRobot
 
 concurrency = 10
-q = Queue(maxsize=1000)
+q = Queue(maxsize=1000000)
 
 logging.basicConfig(format="%(asctime)s: %(message)s", level=logging.INFO)
 AsyncHTTPClient.configure(None, max_clients=1000)
-
-spawn_timestamp = time.time()
-exception_timestamp = time.time()
 
 @coroutine
 def worker():
 	global concurrency, spawn_timestamp, exception_timestamp
 	while True:
 		username = yield q.get()
-		logging.info('[Get] %s' % username)
 
 		try:
 			robot = LessonRobot()
 			result = yield robot.login(username, '123456')
-			if result:
-				logging.info('[Login] %s' % username)
-				count = yield robot.page_count()
-				for i in xrange(count + 1):
-					course_list = yield robot.page(i)
-					course_len = len(course_list)
-					logging.info('[Page] %s: %d/%d => %d' % (username, i, count, course_len))
-					if course_len == 0 and i != 0:
-						raise Exception('Session Expired')
-					for course in course_list:
-						logging.info('[Learn] %s: %s' % (username, course))
-						yield robot.learn(course)
-					now_timestamp = time.time()
-					if now_timestamp - exception_timestamp > 60 and now_timestamp - spawn_timestamp > 60:
-						concurrency += 1
-						IOLoop.current().spawn_callback(worker)
-						spawn_timestamp = now_timestamp
-						logging.info('[Spawn] concurrency = %d' % concurrency)
-				logging.info('[Done] %s' % username)
-			else:
-				logging.info('[Failed] %s' % username)
 		except Exception as e:
 			logging.info('[Exception] %s:%s' % (username, e))
 			yield q.put(username)
-
-			last_timestamp = exception_timestamp
-			exception_timestamp = time.time()
-			if exception_timestamp - last_timestamp < 10:
-				if concurrency > 1:
-					q.task_done()
-					concurrency -= 1
-					logging.info('[Reduce] concurrency = %d' % concurrency)
-					break
-				else:
-					yield sleep(60)
 
 		q.task_done()
 
 @coroutine
 def spawner():
-	for i in xrange(200000, 201000):
+	for i in xrange(24561, 999999):
 		username = 'zjce%06d' % i
 		yield q.put(username)
-		logging.info('[Put] %s' % username)
-		yield sleep(1)
-
-def handler_USR1(signum, frame):
-	global concurrency
-	concurrency += 1
-	logging.info('[Signal] %d: concurrency = %d' % (signum, concurrency))
-	IOLoop.current().spawn_callback(worker)
-
-signal.signal(signal.SIGUSR1, handler_USR1)
 
 @coroutine
 def main():
--- lessonrobot.py
+++ lessonrobot.py
@@ -11,6 +11,10 @@ from tornado.gen import coroutine, sleep, Return
 from tornado.httpclient import AsyncHTTPClient, HTTPError
 from tornado.ioloop import IOLoop
 
+from pymongo import MongoClient
+
+mongoUrl = "mongodb://localhost/region"
+region = MongoClient(mongoUrl)['region']['region']
 
 class LessonRobot(object):
 	referer_url = 'http://www.zjce.gov.cn'
@@ -43,167 +47,15 @@ class LessonRobot(object):
 										body=urlencode(body), follow_redirects=False)
 		except HTTPError as e:
 			r = e.response
-			if 'JSESSIONID' in r.headers['Set-Cookie']:
-				self.session_header['Cookie'] = 'JSESSIONID=' + SimpleCookie(r.headers['Set-Cookie'])['JSESSIONID'].value
-				raise Return(True)
+			url = r.headers['Location']
+			if not 'error' in url:
+				domain = urlparse(url).netloc
+				document = {
+					'username': username,
+					'domain': domain,
+					'url': url
+				}
+				print(username, domain)
+				region.update({'username': username}, document, True)
 			else:
-				raise Return(False)
-
-	@coroutine
-	def page_count(self):
-		r = yield self.fetch_page(1)
-		d = PyQuery(r.body.decode('utf-8'))
-		text = d('.pags_background a').eq(-2).text()
-		reg = r'\.*(\d*)'
-		count = int(re.search(reg, text).group(1))
-
-		raise Return(count)
-
-	@coroutine
-	def page(self, page):
-		course_list = []
-
-		if page == 0:
-			def extract_my(i, e):
-				d = PyQuery(e)
-				course_id = parse_qs(urlparse(d('.my_study_tr a[target]').attr('href')).query)['cwAcademyId'][0]
-				course_list.append(course_id)
-
-			first_res = yield self.client.fetch(self.my_course_url, headers=self.session_header)
-			d = PyQuery(first_res.body.decode('utf-8'))
-			d('body > .right_content_tab_2').each(extract_my)
-
-			my_course_count = len(d('.pags_background')) - 1
-			if my_course_count > 1:
-				batch_res = yield [
-					self.client.fetch(self.my_course_url + '?' + urlencode({'page': i}), headers=self.session_header)
-					for i in range(2, my_course_count + 1)]
-				for r in batch_res:
-					d = PyQuery(r.body.decode('utf-8'))
-					d('body > .right_content_tab_2').each(extract_my)
-
-		# fetch unlearned course
-		if page != 0:
-			def extract_list(i, e):
-				d = PyQuery(e)
-				course_id = parse_qs(urlparse(d('a').attr('href')).query)['cwAcademyId'][0]
-				course_list.append(course_id)
-
-			page_res = yield self.fetch_page(page)
-			d = PyQuery(page_res.body.decode('utf-8'))
-			d('.right_content_tab').each(extract_list)
-
-		raise Return(course_list)
-
-	def fetch_page(self, page):
-		query = {
-			'currentPage': page,
-			'onlyShowNoMylr': 1
-		}
-
-		return self.client.fetch(self.course_list_url + '?' + urlencode(query), headers=self.session_header)
-
-	@coroutine
-	def learn(self, academyId):
-		# fetch course page and register
-		course_res, play_res = yield [self.client.fetch(self.course_url + '?' + urlencode({'cwAcademyId': academyId}),
-														headers=self.session_header),
-									  self.client.fetch(self.play_url + '?' + urlencode({'cwAcademyId': academyId}),
-														headers=self.session_header)]
-
-		d = PyQuery(play_res.body.decode('utf-8'))
-		qs = parse_qs(urlparse(d('#playframe').attr('src')).query)
-		courseID = qs['courseID'][0]
-		userID = qs['userID'][0]
-
-		if 'index.html' in d('#playframe').attr('src'):
-			d = PyQuery(course_res.body.decode('utf-8'))
-			section_status = d('.learning_style01, .learning_style02').map(
-				lambda i, e: ('S%03d' % (i + 1), PyQuery(e).hasClass('learning_style01')))
-
-			# initParam
-			query = {
-				'method': 'initParam',
-				'courseID': courseID,
-				'userID': userID,
-			}
-			yield self.client.fetch(self.progress_url + '?' + urlencode(query), headers=self.session_header)
-
-			@coroutine
-			def learn_section(sid):
-				query = {
-					'method': 'setParam',
-					'lastLocation': 0,
-					'SID': sid,
-					'curtime': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-					'STime': 1,
-					'state': 'S',
-					'courseID': courseID,
-					'userID': userID
-				}
-				yield self.client.fetch(self.progress_url + '?' + urlencode(query), headers=self.session_header)
-				yield sleep(1)
-
-				query = {
-					'method': 'setParam',
-					'lastLocation': randrange(10 ** 7, 10 ** 8),
-					'SID': sid,
-					'curtime': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-					'STime': 1,
-					'state': 'C',
-					'courseID': courseID,
-					'userID': userID
-				}
-				yield self.client.fetch(self.progress_url + '?' + urlencode(query), headers=self.session_header)
-
-			# first learn
-			if len(section_status) == 0:
-				yield learn_section('S001')
-
-				course_res = yield self.client.fetch(self.course_url + '?' + urlencode({'cwAcademyId': academyId}),
-													 headers=self.session_header)
-				d = PyQuery(course_res.body.decode('utf-8'))
-				section_status = d('.learning_style01, .learning_style02').map(
-					lambda i, e: ('S%03d' % (i + 1), PyQuery(e).hasClass('learning_style01')))
-
-			for sid, status in section_status:
-				if not status:
-					yield learn_section(sid)
-		else:
-			d = PyQuery(course_res.body.decode('utf-8'))
-			section_status = d('.learning_style01, .learning_style02').map(
-				lambda i, e: (i + 1, PyQuery(e).hasClass('learning_style01')))
-
-			# scomInitParam
-			query = {
-				'method': 'scomInitParam',
-				'courseID': courseID,
-				'userID': userID,
-			}
-			yield self.client.fetch(self.progress_url + '?' + urlencode(query), headers=self.session_header)
-
-			@coroutine
-			def learn_section(sid):
-				query = {
-					'method': 'scomSetParam',
-					'courseID': courseID,
-					'userID': userID,
-					'SID': 60 * sid,
-					'STime': randrange(6000, 10000),
-					'interval': 60
-				}
-				yield self.client.fetch(self.progress_url + '?' + urlencode(query), headers=self.session_header)
-
-			# first learn
-			if len(section_status) == 0:
-				yield learn_section(1)
-
-				course_res = yield self.client.fetch(self.course_url + '?' + urlencode({'cwAcademyId': academyId}),
-													 headers=self.session_header)
-				d = PyQuery(course_res.body.decode('utf-8'))
-				section_status = d('.learning_style01, .learning_style02').map(
-					lambda i, e: (i + 1, PyQuery(e).hasClass('learning_style01')))
-
-			for sid, status in section_status:
-				if not status:
-					yield learn_section(sid)
+				print('[Failed]', username)
