43b4ac61eb8b59655589bac1522ed06b2643d870
Yuzo <hyzgog@gmail.com>
2016-04-04 01:16:48 +0800

    spec file log

--- killer.py
+++ killer.py
@@ -11,7 +11,7 @@ from lessonrobot import LessonRobot
 concurrency = 10
 q = Queue(maxsize=1000)
 
-logging.basicConfig(format="%(asctime)s: %(message)s", level=logging.INFO)
+logging.basicConfig(format="%(asctime)s: %(message)s", level=logging.INFO, filename='huzhou.log')
 AsyncHTTPClient.configure(None, max_clients=1000)
 
 spawn_timestamp = time.time()
